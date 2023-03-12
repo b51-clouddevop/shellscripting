78d1901d646990746104b8710fe1633d6d4173ef
harshak0405 <harshak0405@gmail.com>
2023-03-12 20:53:45 +0530

    commit

--- robot/componants/frontend.sh
+++ robot/componants/frontend.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 set -e
+
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ] ; then
-    echo -e "\e[31m you must run this script as a root user \e[0m"
-    exit 1
-    fi
-echo -n "installing nginx"
-   if [ $? -eq 0 ]  ; then
-    echo -e "\e[32m succefull \e[0"
-    else 
-    echo -e "\e[31m failure \e[0"
+echo -e "\e[31m you must run this script as a root user \e[0m"
+exit 1
+fi
+echo -n "installing nginx:"
+if [ $? -eq 0 ]  ; then
+echo -e "\e[32m succefull \e[0"
+ else 
+echo -e "\e[31m failure \e[0"
    fi
 yum install nginx -y &>> /tmp/frontend.log
-echo -n "downloading"
- if [ $? -eq 0 ]  ; then
-    echo -e "\e[32m succefull \e[0"
-    else 
-    echo -e "\e[31m failure \e[0"
+echo -n "downloading:"
+ if [ $? -eq 0 ]; then
+echo -e "\e[32m succefull \e[0"
+ else 
+echo -e "\e[31m failure \e[0"
  fi
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-   echo -n "cleaning junks"
-   if [ $? -eq 0 ]  ; then
-   echo -e "\e[32m succefull \e[0"
-    else 
-    echo -e "\e[31m failure \e[0"
-    fi
+echo -n "cleaning junks:"
+if [ $? -eq 0 ]  ; then
+echo -e "\e[32m succefull \e[0"
+else 
+echo -e "\e[31m failure \e[0"
+fi
 rm -rf /usr/share/nginx/html/* &>> /tmp/frontend.log
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
