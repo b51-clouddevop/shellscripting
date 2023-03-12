4e0a1084c93ee274ffd4acf452b7bff3054b4604
harshak0405 <harshak0405@gmail.com>
2023-03-12 21:17:18 +0530

    commit

--- robot/componants/frontend.sh
+++ robot/componants/frontend.sh
@@ -7,26 +7,24 @@ if [ $USERID -ne 0 ] ; then
 echo -e "\e[31m you must run this script as a root user \e[0m"
 exit 1
 fi
-echo -n "installing nginx:"
-if [ $? -eq 0 ]  ; then
-echo -e "\e[32m succefull \e[0"
- else 
-echo -e "\e[31m failure \e[0"
-   fi
-yum install nginx -y &>> /tmp/frontend.log
-echo -n "downloading:"
- if [ $? -eq 0 ]; then
-echo -e "\e[32m succefull \e[0"
- else 
-echo -e "\e[31m failure \e[0"
- fi
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-echo -n "cleaning junks:"
-if [ $? -eq 0 ]  ; then
+stat() {
+    if [ $? -eq 0 ]  ; then
 echo -e "\e[32m succefull \e[0"
 else 
 echo -e "\e[31m failure \e[0"
 fi
+}
+
+echo -n "installing nginx:"
+stat $?
+
+yum install nginx -y &>> /tmp/frontend.log
+echo -n "downloading:"
+ stat $?
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "cleaning junks:"
+stat $?
+
 rm -rf /usr/share/nginx/html/* &>> /tmp/frontend.log
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
