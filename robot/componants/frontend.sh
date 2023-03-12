3c6d968c7c3c9e55b56f2d57e8a009f46b4d12ed
harshak0405 <harshak0405@gmail.com>
2023-03-12 21:56:47 +0530

    commit

--- robot/componants/frontend.sh
+++ robot/componants/frontend.sh
@@ -10,7 +10,7 @@ echo -e "\e[31m you must run this script as a root user \e[0m"
 exit 1
 fi
 stat() {
-    if [ $? -eq 0 ]  ; then
+    if [ $1 -eq 0 ]  ; then
 echo -e "\e[32m succefull \e[0"
 else 
 echo -e "\e[31m failure \e[0"
