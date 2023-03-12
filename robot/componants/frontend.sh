242b6a18c762d9df1057435e6f46bb8f3aa5e5db
harshak0405 <harshak0405@gmail.com>
2023-03-12 20:20:46 +0530

    commit

--- robot/componants/frontend.sh
+++ robot/componants/frontend.sh
@@ -6,12 +6,12 @@ if [ $USERID -ne 0 ] ; then
     echo -e "\e[31m you must run this script as a root user \e[0m"
     exit 1
     fi
-yum install nginx -y
+yum install nginx -y &>> /temp/frontend.log
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> /temp/frontend.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /temp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
