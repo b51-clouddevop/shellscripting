83d17236e6095a164c0254fb88887b0f71824d5a
harshak0405 <harshak0405@gmail.com>
2023-03-14 19:50:59 +0530

    commit

--- robot/componants/frontend.sh
+++ robot/componants/frontend.sh
@@ -8,17 +8,17 @@ echo -n "installing nginx:"
 yum install nginx -y &>> $LOGFILE
 stat $?
 echo -n "downloading:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
  stat $?
 echo -n "cleaning junks:"
 
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 stat $?
 cd /usr/share/nginx/html
-unzip /tmp/$COMPONENTS.zip &>> $LOGFILE
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
 mv frontend-main/* .
 mv static/* .
-rm -rf $COMPONENTS-main README.md
+rm -rf $COMPONENT-main README.md
 echo "configuring reverse proxyfile"
 stat $?
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
