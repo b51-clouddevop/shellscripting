31fc25cbd89b60ba2251877e93dff91c1ad157df
harshak0405 <harshak0405@gmail.com>
2023-03-14 21:35:45 +0530

    commit

--- robot/componants/mongodb.sh
+++ robot/componants/mongodb.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 set -e
-COMPONENT=mongodb
-source componants/common.sh
+     COMPONENT=mongodb
+     source componants/common.sh
 echo -n " confinguring repo: "
-curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> $LOGFILE
+     curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> $LOGFILE
 stat $?
 
 echo -n " installing mangodb: "
-yum install -y mongodb-org &>> $LOGFILE
+     yum install -y mongodb-org &>> $LOGFILE
+stat $?
+echo -n "updating mongodb config:"
+     sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>> $LOGFILE
 stat $?
 
+echo -n "starting mongodb serive: "
+    systemctl enable mongod  &>> $LOGFILE
+    systemctl start mongod   &>> $LOGFILE
+stat $?
+
+echo -n "downloding schema: "
+     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip" &>> $LOGFILE
+     stat $?
+
+echo -n "injecting the schema: "
+     cd /tmp
+     unzip -o mongodb.zip  
+     cd mongodb-main
+     mongo < catalogue.js
+     mongo < users.js
+ stat $?        
