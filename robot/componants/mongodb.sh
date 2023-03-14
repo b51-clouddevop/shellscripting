05aaee2650c38f86749612696112e73372befacc
harshak0405 <harshak0405@gmail.com>
2023-03-14 20:58:20 +0530

    commit

--- robot/componants/mongodb.sh
+++ robot/componants/mongodb.sh
@@ -2,11 +2,11 @@
 set -e
 COMPONENT=mongodb
 source componants/common.sh
-echo -e " confinguring repo: "
+echo -n " confinguring repo: "
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> $LOGFILE
 stat $?
 
-echo -e " installing mangodb: "
+echo -n " installing mangodb: "
 yum install -y mongodb-org &>> $LOGFILE
 stat $?
 
