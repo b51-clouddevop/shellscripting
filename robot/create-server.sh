cf682cb2e8d1590711ae9edb96a7b6c00852c715
harshak0405 <harshak0405@gmail.com>
2023-05-24 19:44:51 +0530

    commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -3,5 +3,5 @@
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')"
 
 echo $AMI_ID
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --key-name MyKeyPair
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq .
 
