fb451cc957a582fd4a52fc9844075f5185bd6a27
harshak0405 <harshak0405@gmail.com>
2023-05-25 21:00:51 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -11,5 +11,4 @@ echo "AMI ID used to lunch instance is: $AMI_ID"
 echo "SGID ID used to lunch instance is: $SGID"
 echo $COMPONENT
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot,SpotOptions=
-{SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}", ---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID ---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | jq
