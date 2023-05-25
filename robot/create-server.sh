3d136387c751a92f2acfd9719b5b29963d6f2ab0
harshak0405 <harshak0405@gmail.com>
2023-05-25 19:53:16 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+COMPONENT=$1
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="sg-0a6535664857652e9"
 echo "AMI ID used to lunch instance is: $AMI_ID"
 echo "SGID ID used to lunch instance is: $SGID"
+echo $COMPONENT
 
 aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]' | jq
