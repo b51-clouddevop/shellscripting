7484785af51770722e259d2053f2f89bbbcec7e3
harshak0405 <harshak0405@gmail.com>
2023-05-25 20:13:33 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -z "$1" ];then
+ echo "component name is requered"
+ exit 1
+fi
 COMPONENT=$1
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="sg-0a6535664857652e9"
