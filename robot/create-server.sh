3635e36ad3c9a0d99ca7cccd0bd1ba107501dffa
harshak0405 <harshak0405@gmail.com>
2023-05-25 19:27:43 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-SGID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=0a6535664857652e9 | jq '.SecurityGroup[].GroupId' | sed -e 's/"//g')"
+SGID="sg-0a6535664857652e9"
 echo "AMI ID used to lunch instance is: $AMI_ID"
-echo "SGID ID used to lunch instance is: $SGID
+echo "SGID ID used to lunch instance is: $SGID"
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq .
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID |jq
