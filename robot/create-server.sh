1ac82a5d32584cd4b329bcf54a26a4e0c8c234f7
harshak0405 <harshak0405@gmail.com>
2023-05-25 19:51:00 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -5,4 +5,4 @@ SGID="sg-0a6535664857652e9"
 echo "AMI ID used to lunch instance is: $AMI_ID"
 echo "SGID ID used to lunch instance is: $SGID"
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID |jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]' | jq
