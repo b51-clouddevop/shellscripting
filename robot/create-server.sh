d9c07c354a62e45dcfc372b9967fe5243544c5b3
harshak0405 <harshak0405@gmail.com>
2023-05-26 20:11:46 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -15,3 +15,6 @@ PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro -
 {SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
+
+echo -n "creating DNS record ********"
+aws route53 change-resource-record-sets --hosted-zone-id Z0661428284PH7J9I27TJ --change-batch file://tmp/dns.json | jq
