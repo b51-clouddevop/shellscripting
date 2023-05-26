31def756164a47969438e71c74d1902af912edb8
harshak0405 <harshak0405@gmail.com>
2023-05-26 19:57:23 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -11,5 +11,7 @@ echo "AMI ID used to lunch instance is: $AMI_ID"
 echo "SGID ID used to lunch instance is: $SGID"
 echo $COMPONENT
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot,SpotOptions=
-{SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot,SpotOptions=
+{SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
