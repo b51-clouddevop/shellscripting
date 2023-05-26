ced89b4b70df5e22f78687b531929eefc0254b36
harshak0405 <harshak0405@gmail.com>
2023-05-26 20:38:48 +0530

    to commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -11,10 +11,21 @@ echo "AMI ID used to lunch instance is: $AMI_ID"
 echo "SGID ID used to lunch instance is: $SGID"
 echo $COMPONENT
 
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot,SpotOptions=
-{SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+create-server() {
 
-sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
+   PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot,SpotOptions=
+   {SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -n "creating DNS record ********"
-aws route53 change-resource-record-sets --hosted-zone-id Z0661428284PH7J9I27TJ --change-batch file:///tmp/dns.json | jq
+   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
+
+   echo -n "creating DNS record ********"
+   aws route53 change-resource-record-sets --hosted-zone-id Z0661428284PH7J9I27TJ --change-batch file:///tmp/dns.json | jq
+}
+if [ "$1" == "all" ]; then
+   for  component in frontend catalogue cart user shipping mangodb mysql rabbitmq redis; do
+      COMPONENT=$component
+      create-server
+   done
+else
+      create-server
+fi
